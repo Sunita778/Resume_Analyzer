ef6a708a4b06bfe7b69d7e68811b0c50497e540b
Sunita778 <sunitapradhan778@gmail.com>
2024-06-18 11:13:38 +0530

    commits

---
+++ main.py
@@ -0,0 +1,14 @@
+# import mysql.connector as conn
+
+# mydb = conn.connect(host = "localhost" , user ="root" , passwd = "Sunita@8457" )
+# cursor = mydb.cursor()
+# cursor.execute("create database sudhanshu")
+
+# s = "create table sudhanshu.ineuron1(employe_id int(10)  , emp_name varchar(80) , emp_mailid varchar(20),emp_salary int(6) , emp_attendence int(3))"
+# q1 = cursor.execute(s)
+
+# q2 = cursor.execute("select * from sudhanshu.ineuron1")
+# print(q1)
+
+# res = cursor.fetchall()
+# print(res)
